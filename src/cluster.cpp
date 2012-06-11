d7de590ff4cd2a1695e3bc35798a722e43facd1c
Dan Steinberg <d.steinberg@acfr.usyd.edu.au>
2012-06-11 11:16:40 +1000

    Small changes before new topic model implementation

--- src/cluster.cpp
+++ src/cluster.cpp
@@ -1,8 +1,4 @@
 // TODO:
-//  - Do i need to store free energy in the SS struct? or can I build it from
-//    the SS themeselves??
-//  - There may be a bug in split() if it encounteres empty clusters, it does
-//    not display the '='.
 //  - Get rid of the copying in the learnVDP and learnGMM functions.
 //  - Some copying still in split()
 
