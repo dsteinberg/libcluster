27b5ce0a145b12b4934a1955222f5db5772e6dd5
dsteinberg <dan.m.steinberg@gmail.com>
2013-03-01 14:21:31 +1100

    Added some python interface files

---
+++ python/pintfctns.h
@@ -0,0 +1,16 @@
+
+#include <stdexcept>
+#include <Eigen/Dense>
+#include <vector>
+//#include <iostream>
+#include <omp.h>
+#include <boost/python.hpp>
+#include <numpy/arrayobject.h>
+#include "libcluster.h"
+#include "distributions.h"
+
+#ifndef PINTFCTNS_H
+#define PINTFCTNS_H
+
+
+#endif
