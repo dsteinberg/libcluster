f3769530035d88f232174ec79b30bdb5ec1cf7b1
Daniel Steinberg <dan.m.steinberg@gmail.com>
2015-01-28 23:59:01 +1100

    think I may have solved the numpy fedora issue

--- FindEigen3.cmake
+++ FindEigen3.cmake
@@ -1,4 +1,8 @@
 # Make sure that we can find Eigen
+# This creates the following variables:
+#  - EIGEN_INCLUDE_DIRS where to find the library
+#  - EIGEN_FOUND TRUE if found, FALSE otherwise
+
 find_path(
   EIGEN_INCLUDE_DIRS Eigen
   /usr/local/eigen3
@@ -6,10 +10,13 @@ find_path(
   /usr/include/eigen3
 )
 
-# Include Eigen
+# Check found Eigen
 if(EIGEN_INCLUDE_DIRS)
-  include_directories(${EIGEN_INCLUDE_DIRS})
+  set(EIGEN_FOUND TRUE)
   message(STATUS "Found Eigen: ${EIGEN_INCLUDE_DIRS}")
 else(EIGEN_INCLUDE_DIRS)
-  message(FATAL_ERROR "Eigen not found")
+  if(EIGEN_FIND_REQUIRED)
+    set(EIGEN_FOUND FALSE)
+    message(FATAL_ERROR "Eigen not found")
+  endif(EIGEN_FIND_REQUIRED)
 endif(EIGEN_INCLUDE_DIRS)
